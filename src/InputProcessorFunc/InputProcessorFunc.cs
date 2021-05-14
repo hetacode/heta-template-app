5014cf1796d5c2657c09911dff4283da8caaf13c
Seba G <sg@hetacode.pl>
2021-05-14 22:18:34 +0200

    Sketch of InputProcessor function - cloning repo

---
+++ src/InputProcessorFunc/InputProcessorFunc.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace InputProcessorFunc
+{
+    public static class InputProcessorFunc
+    {
+        [Function("InputProcessorFunc")]
+        public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("InputProcessorFunc");
+            logger.LogInformation("start");
+
+            var lastCommit = "";
+
+            Repository.Clone("https://github.com/hetacode/heta-template-app.git", "repos/");
+            using var repo = new Repository("repos/.git");
+            // When first checkout
+            if (string.IsNullOrEmpty(lastCommit))
+            {
+                lastCommit = repo.Head.Tip.Tree.Sha;
+                var files = Directory.GetFiles("repos/inputs");
+                foreach (var f in files.Where(w => w.EndsWith(".yaml")))
+                {
+                    // TODO: send to minio storage
+                }
+            }
+            else // Changes from last checkout
+            {
+                foreach (var c in repo.Diff.Compare<TreeChanges>(repo.Lookup<Tree>(lastCommit), repo.Head.Tip.Tree))
+                {
+                    logger.LogInformation($"change: {c.OldPath} - {c.Path}");
+                    // TOOD: take only new files paths: inputs/*
+                }
+            }
+
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+    }
+}
---
+++ src/InputProcessorFunc/readme.md
@@ -0,0 +1,7 @@
+# Run function
+
+## gitlib2 dependency
+Before run function localy you should run below command:
+```console
+export LD_LIBRARY_PATH=$PWD/bin/output/runtimes/ubuntu.18.04-x64/native
+```
