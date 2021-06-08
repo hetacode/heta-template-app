709fe992ef96757a39632def79d0974a49a32dad
Seba G <sg@hetacode.pl>
2021-06-08 23:34:00 +0200

    Http trigger job implementation

--- src/TemplatesPeriodicJob/Jobs/UpdateTemplatesTriggerJob.cs
+++ src/TemplatesPeriodicJob/Jobs/UpdateTemplatesTriggerJob.cs
@@ -1,16 +1,23 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Quartz;
 
 namespace TemplatesPeriodicJob.Jobs
 {
+    [DisallowConcurrentExecution]
     public class UpdateTemplatesTriggerJob : IJob
     {
-        public Task Execute(IJobExecutionContext context)
+        private string TemplateProcessorFuncEndpoint => Environment.GetEnvironmentVariable("TEMPLATE_PROCESSOR_FUNCTION_URL");
+
+        public async Task Execute(IJobExecutionContext context)
         {
-            Console.WriteLine("job call");
-            
-            return Task.CompletedTask;
+            Console.WriteLine($"TemplateProcessorFunc call on {TemplateProcessorFuncEndpoint} url");
+
+            using HttpClient client = new();
+            var result = await client.GetAsync(TemplateProcessorFuncEndpoint);
+
+            Console.WriteLine($"end TemplateProcessorFunc with {result.StatusCode} status code");
         }
     }
 }
