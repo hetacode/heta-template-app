64b7f6f370fbad0d79a856f255a5772910a22763
Seba G <sg@hetacode.pl>
2021-07-24 20:13:30 +0200

    Add helm chart minio depoyment with configuration job - add metallb operator in order to give ability to create load balancer service - minio should be expose externally via load balancer service - fixed redpanda service - set proper selector

--- scripts/k8s-kind-init-app-arch.sh
+++ scripts/k8s-kind-init-app-arch.sh
@@ -16,6 +16,20 @@ helm install \
   --wait \
   --set installCRDs=true
 
+KIND_K8S_IP=$(kubectl get endpoints kubernetes -n default -o=jsonpath='{.subsets[0].addresses[0].ip}')
+
+helm repo add metallb https://metallb.github.io/metallb
+rm metallb-values.yaml
+cat > metallb-values.yaml << EOL
+configInline:
+  address-pools:
+   - name: metal-lb-ip-pool
+     protocol: layer2
+     addresses:
+     - $KIND_K8S_IP/32
+EOL
+helm install metallb metallb/metallb -f metallb-values.yaml --namespace metallb-system --create-namespace --wait
+
 # Wait for ready cert-manager-webhook pod
 # while [ "$(kubectl get pods -l=app='webhook' -o jsonpath='{.items[*].status.containerStatuses[0].ready}' -n cert-manager)" != "true" ]; do
 #    sleep 5
@@ -42,3 +56,4 @@ kubectl apply \
   -f k8s_configs/redpanda_one_node_cluster.yaml \
   -f k8s_configs/redpanda_service.yaml
 
+echo "IP address: $KIND_K8S_IP"
